e25fc6797fb2507390c322a2fdc9cd0d16e65a50
James Esteban <james.esteban08@gmail.com>
2025-06-24 17:12:04 +0800

    edit routing

--- app/(tabs)/test.tsx
+++
@@ -1,9 +0,0 @@
-import { Text, View } from 'react-native';
-
-export default function TestScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>This is the middle tab!</Text>
-    </View>
-  );
-}
