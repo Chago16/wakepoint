75122fc912247e85d4436d088cc774ae5f8e5053
James Esteban <james.esteban08@gmail.com>
2025-06-25 09:42:09 +0800

    remove dark theme

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,5 +1,5 @@
 const tintColorLight = '#145E4D';
-const tintColorDark = '#70AF76';
+const tintColorDark = '#145E4D';
 
 export const Colors = {
   light: {
@@ -36,35 +36,35 @@ export const Colors = {
   },
 
   dark: {
-    text: '#FAF9F7',
-    background: '#161A1A',
+    text: '#2A3435',
+    background: '#FAF9F7',
 
     // Primary theme colors
-    tint: tintColorDark,
-    icon: '#77837A',
+    tint: tintColorLight,
+    icon: '#B6C6C3',
 
     // Tab bar
-    tabIconDefault: '#CFC8F3',
-    tabIconSelected: tintColorDark,
-    tabBarBackground: '#1E1E1E',
+    tabIconDefault: '#BBBABC',
+    tabIconSelected: tintColorLight,
+    tabBarBackground: '#FFFFFF',
 
     // Buttons
-    buttonBackground: tintColorDark,
-    buttonText: '#000000',
-    buttonBorder: '#497354',
+    buttonBackground: tintColorLight,
+    buttonText: '#FFFFFF',
+    buttonBorder: '#1F3E35',
 
     // Selections / Highlights
-    selectedBackground: '#2C3F36',
-    selectedText: tintColorDark,
+    selectedBackground: '#DFF0E7',
+    selectedText: tintColorLight,
 
     // Cards / Surfaces
-    cardBackground: '#1E1E1E',
-    cardBorder: '#2A2A2A',
+    cardBackground: '#FFFFFF',
+    cardBorder: '#E0E0E0',
 
     // Inputs
-    inputBackground: '#2C2C2C',
-    inputText: '#FFFFFF',
-    inputBorder: '#444444',
-    inputPlaceholder: '#AAAAAA',
+    inputBackground: '#FFFFFF',
+    inputText: '#2A3435',
+    inputBorder: '#CCCCCC',
+    inputPlaceholder: '#999999',
   },
 };
